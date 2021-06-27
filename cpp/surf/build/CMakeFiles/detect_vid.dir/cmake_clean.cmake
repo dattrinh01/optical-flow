b6492ca2ab9ff628fcc3db44eb4cf34dd4296fe6
dattrinh01 <tandattrinh2001@gmail.com>
2021-06-27 18:13:12 +0700

    Update-all-code-include-cpp-and-python

---
+++ cpp/surf/build/CMakeFiles/detect_vid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_vid.dir/surf_detector_video.cpp.o"
+  "detect_vid.pdb"
+  "detect_vid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_vid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
